ea30acdad8d6e4931e81656371cd37efff40bedd
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-08-07 16:21:54 +0100

    Set up the header and the search

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import { View } from "react-native";
+import FilterButton from "../components/FilterButton";
 import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
 
 export default function Index() {
     return (
         <View>
             <Header />
+            <SearchBar />
+            <FilterButton />
         </View>
     );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,31 +1,28 @@
-import { TextInput, StyleSheet } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 
 interface SearchBarProps {
-  value: string;
-  onChangeText: (text: string) => void;
+    value: string;
+    onChangeText: (text: string) => void;
 }
 
-export default function SearchBar({
-  value,
-  onChangeText,
-}: SearchBarProps) {
-  return (
-    <TextInput
-      style={styles.input}
-      placeholder="Rechercher un colis ou un véhicule..."
-      value={value}
-      onChangeText={onChangeText}
-    />
-  );
+export default function SearchBar({ value, onChangeText }: SearchBarProps) {
+    return (
+        <TextInput
+            style={styles.input}
+            placeholder="Rechercher un colis ou un véhicule..."
+            value={value}
+            onChangeText={onChangeText}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    backgroundColor: "#FFFFFF",
-    margin: 16,
-    padding: 12,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#D1D5DB",
-  },
+    input: {
+        backgroundColor: "#FFFFFF",
+        margin: 16,
+        padding: 12,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#D1D5DB",
+    },
 });
